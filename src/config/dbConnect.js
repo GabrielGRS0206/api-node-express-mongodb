7ec44f92f4c47c52ddd74bbccef9cf79b8f19b48
Gabriel Rocha <44039603+GabrielGRS0206@users.noreply.github.com>
2022-04-05 11:22:00 -0300

    Update dbConnect.js

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,8 +1,6 @@
 import mongoose from "mongoose"
 
-//mongoose.connect("mongodb+srv://alura:123@alura.dkjed.mongodb.net/alura-node");
 mongoose.connect("mongodb+srv://grocha:master0206@cluster0.wqfdt.mongodb.net/mongoDb?retryWrites=true&w=majority");
-
 
 let db = mongoose.connection;
 
